a65fd6f651c7612f43bfdbb140a9bab3bdc26242
Thiyanesh <thiyanesh7777@gmail.com>
2025-08-23 06:03:55 +0530

    Initial commit

---
+++ Day-1.py
@@ -0,0 +1,7 @@
+class Result:
+    def userInput(self):
+        num = int(input())
+        print(num)
+
+r = Result()
+r.userInput()
---
+++ Day-2.py
@@ -0,0 +1,10 @@
+i = 5
+f = 7.5
+b = True
+s = "Rolex"
+
+
+print(type(i))
+print(type(f))
+print(type(b))
+print(type(s))
